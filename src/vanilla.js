8cebde0b8ebd6cc890f31b16fc4486789c02e613
cyril-git-h <cyril.guzikov@gmail.com>
2021-05-05 05:38:51 +0400

    make header responsive and change social icons

--- src/vanilla.js
+++ src/vanilla.js
@@ -51,9 +51,61 @@ export const smoothScrollFunction = () => {
     event.preventDefault();
     const targetId = this.getAttribute("href");
     let offsetTop = document.querySelector(targetId).offsetTop;
-    window.scroll({
+
+    if (!document.querySelector(".banner").classList.contains("hidden")) {
+      document.querySelector(".banner").classList.add("hidden");
+      return setTimeout(() => {
+        let offsetTop = document.querySelector(targetId).offsetTop;
+        window.scroll({
+          top: offsetTop - 110,
+          behavior: "smooth",
+        });
+      }, 1000);
+    }
+
+    return window.scroll({
       top: offsetTop - 110,
       behavior: "smooth",
+    });
+  }
+};
+
+export const preventScroll = () => {
+  let touchStart = 0;
+  let touchEnd = 0;
+  let banner = document.querySelector(".banner");
+
+  if (!banner) return;
+
+  banner.classList.remove("hidden");
+
+  window.addEventListener("unload", () => {
+    window.scroll({
+      top: 0,
+    });
+  });
+
+
+  document.addEventListener("touchstart", (event) => {
+    touchStart = event.changedTouches[0].clientY;
+  });
+  document.addEventListener("touchend", (event) => {
+    touchEnd = event.changedTouches[0].clientY;
+    if (touchEnd > touchStart) {
+      performScroll(1);
+    }
+    return performScroll(-1);
+  });
+
+  function performScroll(value) {
+    if (value == 1 && window.pageYOffset <= 492) {
+      banner.classList.remove("hidden");
+    }
+
+    window.addEventListener("scroll", () => {
+      if (value == -1 && window.pageYOffset <= 492) {
+        banner.classList.add("hidden");
+      }
     });
   }
 };
